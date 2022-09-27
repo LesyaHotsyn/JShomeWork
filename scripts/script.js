fa4ee9a78f6956710092c8443d3cfa61e19707af
= <85835454+LesyaHotsyn@users.noreply.github.com>
2022-09-27 19:01:28 +0300

    add first task

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+let ageUser;
+let firstNameUser;
+let lastNameUser;
+
+let newUser;
+let shoppingList;
+let onlineStatusUser;
+let employeeSalary;
+let cursorCoordinates;
+
+console.log('Hello World!!!');
+
