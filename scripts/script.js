1110f20aec7c937251d8f2cb4440751060e9993f
= <85835454+LesyaHotsyn@users.noreply.github.com>
2022-10-03 17:52:17 +0300

    completed task 2

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,10 @@
 let firstNumber;
 let secondNumber;
+let arithmeticOperation;
+let sum;
+let subtraction;
+let division;
+let multiplication;
 
 firstNumber = prompt("Введіть перше число");
 secondNumber = prompt("Введіть друге число");
@@ -10,4 +15,28 @@ if (firstNumber == secondNumber) {
     alert("Число " + firstNumber + "  більше, ніж число  " + secondNumber + "!");
 } else {
     alert("Число " + secondNumber + "  більше, ніж число  " + firstNumber + "!");
+}
+
+
+arithmeticOperation = prompt("Введіть дію над числами. Це може бути +, -, / чи *");
+
+switch (arithmeticOperation) {
+    case "+":
+        sum = firstNumber + secondNumber;
+        alert("Сума чисел дорівнює: " + sum);
+        break;
+    case "-":
+        subtraction = firstNumber - secondNumber;
+        alert("  " + subtraction);
+        break;
+    case "/":
+        division = firstNumber / secondNumber;
+        alert("  " + division);
+        break;
+    case "*":
+        multiplication = firstNumber * secondNumber;
+        alert("  " + multiplication);
+        break;    
+    default: 
+        alert("Дію введено не коректно. Спробуйте ще раз!");
 }
