db21c84439b0b8717b0a6992e75b89f707384f27
= <85835454+LesyaHotsyn@users.noreply.github.com>
2022-10-03 17:27:10 +0300

    fixed

--- scripts/script.js
+++ scripts/script.js
@@ -2,12 +2,12 @@ let firstNumber;
 let secondNumber;
 
 firstNumber = prompt("Введіть перше число");
-secondNumber = prompt("Введіть друге число")ж
+secondNumber = prompt("Введіть друге число");
 
 if (firstNumber == secondNumber) {
     alert("Числа рівні");
 } else if (firstNumber > secondNumber) {
-    alert("Число" + firstNumber + "більше!")
+    alert("Число " + firstNumber + "  більше, ніж число  " + secondNumber + "!");
 } else {
-    alert("Число" + secondNumber + "більше!")
+    alert("Число " + secondNumber + "  більше, ніж число  " + firstNumber + "!");
 }
